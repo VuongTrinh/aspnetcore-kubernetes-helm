471bf88ea52abeeb456e590db5470577fb2e9a21
Vuong Xuan Trinh <txv@aod.vn>
2022-03-10 17:15:33 +0700

    Run ASP.NET Core 3 on Kubernetes with Helm.

---
+++ app/WebApi/Controllers/InfoController.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+
+namespace WebApi.Controllers
+{
+    [ApiController]
+    [Route("")]
+    public class InfoController : ControllerBase
+    {
+        private readonly ILogger<InfoController> _logger;
+
+        public InfoController(ILogger<InfoController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public InfoModel GetInfo()
+        {
+            return new InfoModel { AppEnvironment = GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT"), AppHost = GetEnvironmentVariable("APPHOST") };
+        }
+
+        private string GetEnvironmentVariable(string name)
+        {
+            _logger.LogInformation($"Getting environment variable '{name}'.");
+            return Environment.GetEnvironmentVariable(name.ToLower()) ?? Environment.GetEnvironmentVariable(name.ToUpper());
+        }
+    }
+}
---
+++ app/WebApi/InfoModel.cs
@@ -0,0 +1,8 @@
+﻿namespace WebApi
+{
+    public class InfoModel
+    {
+        public string AppEnvironment { get; set; }
+        public string AppHost { get; set; }
+    }
+}
